8284199727ecc8f03e8703d1339ef56134c32930
PedroLongue <pedrolongue13@gmail.com>
2023-11-19 22:10:49 -0300

    Feat: Add form validation using react-hook-form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,22 +1,80 @@
+"use client"
+
 import React from 'react';
-import Logo from '../components/logo/page'
-import './register.css'
+import { useForm } from 'react-hook-form';
+import Logo from '../components/logo/page';
+import './register.css';
 import Link from 'next/link';
 
-// TELA DE PERFIL
+interface FormData {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Register: React.FC = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<FormData>();
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    // Lógica de envio aqui
+  };
+
+  // Regex para verificar se a senha contém pelo menos um número e um caractere especial
+  const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])/;
+
+  // Função para validar se as senhas coincidem
+  const validatePassword = (value: string) => {
+    return value === watch('password') || 'As senhas não coincidem';
+  }
+
   return (
     <div className='register-page-wrapper'>
         <Logo />
         <div className='register-form-wrapper'>
-          <form className='register-form' action="">
+          <form className='register-form' onSubmit={handleSubmit(onSubmit)}>
             <h2>Cadastre-se</h2>
-            <input placeholder='Nome completo' className='register-form-input' type="text" />
-            <input placeholder='Email' className='register-form-input' type="email" />
-            <input placeholder='Senha' className='register-form-input' type="password" />
-            <input placeholder='Confirmar senha' className='register-form-input' type="password" />
-            <input className='register-form-button register-form-input' type="submit" value="Cadastrar" />
+            <input
+              placeholder='Nome completo'
+              className='register-form-input'
+              type="text"
+              {...register('fullName', { required: 'Nome completo é obrigatório' })}
+            />
+            {errors.fullName && <span className='error-message'>{errors.fullName.message}</span>}
+
+            <input
+              placeholder='Email'
+              className='register-form-input'
+              type="email"
+              {...register('email', { required: 'Email é obrigatório', pattern: { value: /^\S+@\S+\.\S+$/, message: 'Email inválido' } })}
+            />
+            {errors.email && <span className='error-message'>{errors.email.message}</span>}
+
+            <input
+              placeholder='Senha'
+              className='register-form-input'
+              type="password"
+              {...register('password', { 
+                required: 'Senha é obrigatória', 
+                minLength: { value: 6, message: 'A senha deve ter pelo menos 6 caracteres' },
+                pattern: { value: passwordRegex, message: 'A senha deve conter pelo menos um número e um caractere especial' }
+              })}
+            />
+            {errors.password && <span className='error-message'>{errors.password.message}</span>}
+
+            <input
+              placeholder='Confirmar senha'
+              className='register-form-input'
+              type="password"
+              {...register('confirmPassword', { required: 'Confirmação de senha é obrigatória', validate: validatePassword })}
+            />
+            {errors.confirmPassword && <span className='error-message'>{errors.confirmPassword.message}</span>}
+
+            <input
+              className='register-form-button register-form-input'
+              type="submit"
+              value="Cadastrar"
+            />
           </form>
           <span>Já tem cadastro? <Link href="/login">Conecte-se</Link></span>
         </div>
