063c8ce4c7ccfda7f49d86506f745162b429efba
João Vitral <joaoviniciusvitral@hotmail.com>
2023-11-24 15:44:44 -0300

    Fix placeholder color and background color in home.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,39 @@
-import { AppProvider } from "@/contexts/contextApi";
-import "@/styles/globals.css";
-import "@/styles/home.css";
-import "@/styles/register.css";
-import "@/styles/login.css";
+import type { Metadata } from "next"
+import { Inter, Montserrat } from "next/font/google"
 
-import type { Metadata } from "next";
-import { Inter, Montserrat } from "next/font/google";
+import { AppProvider } from "@/contexts/contextApi"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import "@/styles/globals.css"
+import "@/styles/home.css"
+import "@/styles/register.css"
+import "@/styles/login.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 const montserrat = Montserrat({
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "NextAuto",
   description: "Sistema de reserva de passagem de ônibus",
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="pt-br">
       <AppProvider>
         <body className={`${inter.className} ${montserrat.className}`}>
+          <Header />
           {children}
+          <Footer />
         </body>
       </AppProvider>
     </html>
-  );
+  )
 }
