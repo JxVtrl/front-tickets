0fe43adc085a4b09962f7352fa444f725984883f
João Vitral <joaoviniciusvitral@hotmail.com>
2023-11-25 00:56:12 -0300

    Add ClockIcon and TravelTime components

--- src/components/Selected/components/Header/components/DateShow/index.tsx
+++ src/components/Selected/components/Header/components/DateShow/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 import CalendarIcon from "@/assets/icons/CalendarIcon"
 import { useApp } from "@/contexts/contextApi"
-import { format_date } from "@/utils/functions"
+import { format_date, format_hour } from "@/utils/functions"
 import React from "react"
 
 const DateShow: React.FC = () => {
@@ -13,7 +13,7 @@ const DateShow: React.FC = () => {
     <div className="flex items-center gap-[4px]">
       <CalendarIcon />
       <span className="text-[#A0A0A0] text-sm mx-[4px]">
-        {format_date(selectedRoute.data_ida)} - {selectedRoute?.hora_ida}
+        {format_date(selectedRoute.data_ida)} - {format_hour(selectedRoute?.hora_ida)}
       </span>
     </div>
   )
