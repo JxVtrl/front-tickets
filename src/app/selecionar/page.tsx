ef913242c2e4dce00126be7dd28dd2fb0be18779
João Vitral <joaoviniciusvitral@hotmail.com>
2023-11-25 01:57:30 -0300

    Fix bugs and update dependencies

--- src/app/selecionar/page.tsx
+++ src/app/selecionar/page.tsx
@@ -2,19 +2,17 @@
 
 import Selected from "@/components/Selected"
 import { useApp } from "@/contexts/contextApi"
-import { usePathname } from "next/navigation"
+import { useParams, usePathname,useSearchParams } from "next/navigation"
 import React from "react"
 
 const Page: React.FC = () => {
   const { rotas,setSelectedRoute } = useApp()
   
-  const pathname = usePathname()
+  const searchParams = useSearchParams()
+  const id = searchParams.get("id")
   
-  const pageQuery = new URLSearchParams(pathname)
-  const id = Number(pageQuery.get("id"))
-
   React.useEffect(() => {
-    const route = rotas.find((rota) => rota.id === id)
+    const route = rotas.find((rota) => rota.id === Number(id))
     setSelectedRoute(route || null)
   }, [rotas,id])
 
--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -120,19 +120,22 @@ export const criar_rotas = () => {
       Math.floor(Math.random() * cidades.length)
     ]
     
-    let random_day = Math.floor(Math.random() * 30) + 1
-    let random_month = Math.floor(Math.random() * 12) + 1
+    let random_day = (Math.floor(Math.random() * 30) + 1).toString().padStart(2, "0")
+    let random_month = (Math.floor(Math.random() * 12) + 1).toString().padStart(2, "0")
     let random_year = Math.floor(Math.random() * 5) + 2023
+    
+    let data_ida = `${random_day}/${random_month}/${random_year}`
+    let data_chegada = `${random_day}/${random_month}/${random_year}`
     
     let rota: Rota = {
       id: i,
       origem,
       destino,
-      data_ida: `${random_day}/${random_month}/${random_year}`,
+      data_ida,
       hora_ida: `${Math.floor(Math.random() * 24)}:${Math.floor(
         Math.random() * 60
       )}`,
-      data_chegada: `${random_day}/${random_month}/${random_year}`,
+      data_chegada,
       hora_chegada: `${Math.floor(Math.random() * 24)}:${Math.floor(
         Math.random() * 60
       )}`,
