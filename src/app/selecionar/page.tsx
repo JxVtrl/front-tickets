9a1785c5dcbafae2d481569d7ed2e72cc1197d96
João Vitral <joaoviniciusvitral@hotmail.com>
2023-11-25 02:00:31 -0300

    Fix bug in page.tsx

--- src/app/selecionar/page.tsx
+++ src/app/selecionar/page.tsx
@@ -2,19 +2,29 @@
 
 import Selected from "@/components/Selected"
 import { useApp } from "@/contexts/contextApi"
-import { useSearchParams } from "next/navigation"
+import { useRouter, useSearchParams } from "next/navigation"
 import React from "react"
 
 const Page: React.FC = () => {
-  const { rotas,setSelectedRoute } = useApp()
-  
+  const { rotas, setSelectedRoute } = useApp()
+
   const searchParams = useSearchParams()
   const id = searchParams.get("id")
-  
+
+  const router = useRouter()
+
   React.useEffect(() => {
+    if (!id) return
+
     const route = rotas.find((rota) => rota.id === Number(id))
-    setSelectedRoute(route || null)
-  }, [rotas,id])
+
+    if (!route) {
+      router.push("/")
+      return
+    } else {
+      setSelectedRoute(route)
+    }
+  }, [rotas, id])
 
   return <Selected />
 }
